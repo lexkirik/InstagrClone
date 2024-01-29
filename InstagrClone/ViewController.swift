d762a1ff1afb81ef0ceba507bfc6e4541b886d32
Aliaksei Kirykovich <lexkirik@mail.ru>
2024-01-29 11:21:33 +0300

    created user interface

---
+++ InstagrClone/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  InstagrClone
+//
+//  Created by Test on 29.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signInClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
+    
+
+}
+
