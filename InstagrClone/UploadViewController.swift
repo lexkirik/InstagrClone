3cf7ce7f309457cab610a1a142a2ff9a338f14dc
Aliaksei Kirykovich <lexkirik@mail.ru>
2024-01-29 16:22:53 +0300

    upload photos using PHPicker

--- InstagrClone/UploadViewController.swift
+++ InstagrClone/UploadViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 
-class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, PHPickerViewControllerDelegate {
+
+    
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var commentText: UITextField!
@@ -22,15 +26,40 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @objc func chooseImage() {
-        let imagePicker = UIImagePickerController()
+        var imageCongiguration = PHPickerConfiguration(photoLibrary: .shared())
+        imageCongiguration.selectionLimit = 1
+        imageCongiguration.filter = PHPickerFilter.any(of: [.images, .livePhotos])
+        let imagePicker = PHPickerViewController(configuration: imageCongiguration)
         imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
+        
+        // imagePicker using .photoLibrary will be deprecated soon
+//        let imagePicker = UIImagePickerController()
+//        imagePicker.delegate = self
+//        imagePicker.sourceType = .photoLibrary
+//        present(imagePicker, animated: true, completion: nil)
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        imageView.image = info[.originalImage] as? UIImage
-        self.dismiss(animated: true, completion: nil)
+    // imagePicker using .photoLibrary will be deprecated soon
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        imageView.image = info[.originalImage] as? UIImage
+//        self.dismiss(animated: true, completion: nil)
+//    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        results.forEach { result in
+            result.itemProvider.loadObject(ofClass: UIImage.self) { reading, error in
+                guard let image = reading as? UIImage, error == nil else {return}
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                }
+//                result.itemProvider.loadFileRepresentation(forTypeIdentifier: "publc.image") { <#URL?#>, <#Error?#> in
+//                    <#code#>
+//                }
+            }
+        }
     }
     
     @IBAction func uploadButtonClicked(_ sender: Any) {
