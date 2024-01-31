eddf1f4c7957b0022500f11107b2240b8986c610
Aliaksei Kirykovich <lexkirik@mail.ru>
2024-01-31 08:34:09 +0300

    return from Upload to Feed after uploading photo

--- InstagrClone/UploadViewController.swift
+++ InstagrClone/UploadViewController.swift
@@ -87,11 +87,15 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
                             //database
                             let firestoreDatabase = Firestore.firestore()
                             var firestoreReference: DocumentReference? = nil
-                            let firestorePost = ["imageURL" : imageURL!, "postedBy" : Auth.auth().currentUser!.email!, "postComent" : self.commentText.text!, "date" : "date", "likes" : 0] as [String : Any]
+                            let firestorePost = ["imageURL" : imageURL!, "postedBy" : Auth.auth().currentUser!.email!, "postComent" : self.commentText.text!, "date" : FieldValue.serverTimestamp(), "likes" : 0] as [String : Any]
                             
                             firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { error in
                                 if error != nil {
                                     self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                                } else {
+                                    self.imageView.image = UIImage(named: "upload.png")
+                                    self.commentText.text = ""
+                                    self.tabBarController?.selectedIndex = 0
                                 }
                             })
                         }
